1bafcfaf0411fd22e714213df03ce9dbe3d7a6cf
Luis <sanchez.fernando9821@gmail.com>
2023-01-04 13:28:56 -0600

    ocultar componenetes

--- funciones.js
+++ funciones.js
@@ -1,9 +1,17 @@
 function encriptar(){
     texto= document.getElementById("textoEntrada").value
+    if (texto=="") {
+        document.getElementById("copiador").style.visibility = 'hidden';
+        document.getElementById('texto1').style.display = 'block';
+        document.getElementById('texto2').style.display = 'block';
+        document.getElementById('fotoL').style.display = 'block';    
+        document.getElementById('textoSalida').style.display = 'none';
+        return
+    }
     document.getElementById("copiador").style.visibility = 'visible';
-    document.getElementById('texto1').style.visibility = 'hidden';
-    //document.getElementById('texto2').style.visibility = 'hidden';
-    document.getElementById('fotoL').style.visibility = 'hidden';
+    document.getElementById('texto1').style.display = 'none';
+    document.getElementById('texto2').style.display = 'none';
+    document.getElementById('fotoL').style.display = 'none';
     let salida=""
     for (let i = 0; i < texto.length; i++) {
         switch(texto.charAt(i)){
@@ -28,18 +36,32 @@ function encriptar(){
         }
     }
     console.log(salida)
-    document.getElementById("texto2").value=salida
+    document.getElementById('textoSalida').style.display = 'block';
+    document.getElementById("textoSalida").value=salida
     
 }
 
 function desencriptar(){
     let salida=document.getElementById("textoEntrada").value
+    if (salida=="") {
+        document.getElementById("copiador").style.visibility = 'hidden';
+        document.getElementById('texto1').style.display = 'block';
+        document.getElementById('texto2').style.display = 'block';
+        document.getElementById('fotoL').style.display = 'block';    
+        document.getElementById('textoSalida').style.display = 'none';
+        return
+    }
+    document.getElementById("copiador").style.visibility = 'visible';
+    document.getElementById('texto1').style.display = 'none';
+    document.getElementById('texto2').style.display = 'none';
+    document.getElementById('fotoL').style.display = 'none';
     salida=salida.replaceAll('ai','a');
     salida=salida.replaceAll('enter','e');
     salida=salida.replaceAll('imes','i');
     salida=salida.replaceAll('ober','o');
     salida=salida.replaceAll('ufat','u');
     console.log(salida)
+    document.getElementById('textoSalida').style.display = 'block';
     document.getElementById("textoSalida").value=salida
     
 }
